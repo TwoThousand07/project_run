0c54b9d428408a9fc082ebab379dc3db933a4ffa
TwoThousand07 <aytimov.erzhanae@gmail.com>
2025-11-08 22:18:25 +0500

    [feat]: add RunViewSet

--- app_run/views.py
+++ app_run/views.py
@@ -3,15 +3,31 @@ from django.conf import settings
 
 
 from rest_framework import views
+from rest_framework import viewsets
 from rest_framework.response import Response
 
 
+from .serializers import RunSerializer
+from .models import Run
+
 
 class CompanyInformationAPIView(views.APIView):
-    
+    '''
+        Инфорация о странице
+    '''
     def get(self, request):
         return Response({
             "company_name": settings.COMPANY_NAME,
             "slogan": settings.SLOGAN,
             "contacts": settings.CONTACTS
         })
+        
+    
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
+    
+
+        
+
+
