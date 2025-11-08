dd31a1e2a029242b487f2518e6ee1a26050b11ca
TwoThousand07 <aytimov.erzhanae@gmail.com>
2025-11-08 22:14:58 +0500

    [feat]: Add Run model

--- app_run/models.py
+++ app_run/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+
+class Run(models.Model):
+    athlete = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(blank=True)
+    
+    
