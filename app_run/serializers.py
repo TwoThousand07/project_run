99cb49bece2788e1addb7a656b5badf6672c160c
TwoThousand07 <aytimov.erzhanae@gmail.com>
2025-11-08 23:11:39 +0500

    feat: add UserSerializer to serializers.py

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
+from django.contrib.auth.models import User
+
 from .models import Run
 
 
 class RunSerializer(serializers.ModelSerializer):
     class Meta:
         model = Run
+        fields = "__all__"
+        
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
         fields = "__all__"
