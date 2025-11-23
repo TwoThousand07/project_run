5ffb700eafd0985f9d8fbce98317d53b7c85e0b6
TwoThousand07 <aytimov.erzhanae@gmail.com>
2025-11-23 17:09:06 +0500

    feat: register ChallengeViewSet in the URL router

--- app_run/urls.py
+++ app_run/urls.py
@@ -7,7 +7,7 @@ from rest_framework.routers import SimpleRouter
 router = SimpleRouter()
 router.register(r"runs", RunViewSet)
 router.register(r"users", UserViewSet, basename="users")
-
+router.register(r"challenges", ChallengeViewSet)
 
 
 
