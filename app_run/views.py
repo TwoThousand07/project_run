80389c4c41499546af66254f0cb8bc3c6eea8540
TwoThousand07 <aytimov.erzhanae@gmail.com>
2025-11-23 17:37:44 +0500

    feat: create Challenge instance after finishing 10 runs with status "finished"

--- app_run/views.py
+++ app_run/views.py
@@ -82,7 +82,7 @@ class RunStopAPIView(APIView):
 
         run.status = "finished"
         run.save()
-        if Run.objects.filter(athlete=run.athlete).count() == 10:
+        if Run.objects.filter(athlete=run.athlete, status="finished").count() == 10:
             Challenge.objects.create(athlete=run.athlete, full_name="Сделай 10 Забегов!")
         return Response({"message": "Забег успешно завершен!"}, status=status.HTTP_200_OK)
 
