d2a3525626dfb809d97cefc476b6514458ed76cd
TwoThousand07 <aytimov.erzhanae@gmail.com>
2025-11-23 17:11:00 +0500

    feat: register Challenge model in admin and create migration for it

--- app_run/admin.py
+++ app_run/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Run, AthleteInfo
+from .models import Run, AthleteInfo, Challenge
 
 admin.site.register(Run)
 admin.site.register(AthleteInfo)
+admin.site.register(Challenge)
 
 # @admin.register(User)
 # class UserAdmin(admin.ModelAdmin):
