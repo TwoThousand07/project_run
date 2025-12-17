393bcda27a81d81e1ada6211a1032774b0a8a7d7
TwoThousand07 <aytimov.erzhanae@gmail.com>
2025-12-17 20:54:02 +0500

    fix: update distance calculation to return kilometers instead of meters

--- app_run/services.py
+++ app_run/services.py
@@ -7,4 +7,4 @@ def calculate_distance_between_two_positions(start_pos: Position, end_pos: Posit
     start = (start_pos.latitude, start_pos.longitude)
     end = (end_pos.latitude, end_pos.longitude)
 
-    return geodesic(start, end).meters
+    return geodesic(start, end).km
